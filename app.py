ab39c2d33ffd4f69ffdaf4ed7f3c1d0e9a6e55cc
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2023-05-11 23:45:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 
-def cells_per_ml():
-    # Introducción manual del número de células contadas y el factor de dilución
-    cells_counted = st.number_input("Introduce el número de células contadas por cuadrante: ")
-    dilution_factor = st.number_input("Introduce el factor de dilución de la muestra: ")
-    
-    # Cálculo del número de células por mililitro
-    cells_per_mm2 = cells_counted * 4  # Se cuentan las células en 4 cuadrantes
-    cells_per_ml = cells_per_mm2 * 10**4 * dilution_factor
-    
-    # Gráfica de los datos ingresados
-    days = st.text_input("Ingrese los días contados separados por comas:")
-    days_list = [int(x) for x in days.split(",")]
-    plt.plot(days_list, [cells_per_ml]*len(days_list), 'ro')
-    plt.xlabel('Días')
-    plt.ylabel('Número de células')
-    st.sidebar.pyplot()
-    
-    return cells_per_ml
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
 
-# Crear una gráfica vacía en la barra lateral
-st.sidebar.subheader("Gráfica de células por día")
-plt.figure()
-st.sidebar.pyplot()
-
-# Ejemplo de uso
-result = cells_per_ml()
-st.write(f"El número de células por mililitro es: {result}")
-
-
-
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
 
 
 
