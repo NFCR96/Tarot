d9074be3c2e4100a4db1f8ad0cef8d38d9ae1fa5
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2023-05-11 23:34:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 import streamlit as st
-import streamlit as st
-import folium
 
-# Creamos una función para generar el mapa
-def generate_map():
-    # Creamos el mapa centrado en una coordenada específica
-    mapa = folium.Map(location=[19.4326, -99.1332], zoom_start=10)
+def cells_per_ml(cells_counted, dilution_factor):
+    # Cálculo del número de células por mililitro
+    cells_per_mm2 = cells_counted * 4  # Se cuentan las células en 4 cuadrantes
+    cells_per_ml = cells_per_mm2 * 10**4 * dilution_factor
+    return cells_per_ml
 
-    # Añadimos un marcador al mapa
-    folium.Marker(location=[19.4326, -99.1332], popup="Ciudad de México").add_to(mapa)
-
-    # Convertimos el mapa a HTML para mostrarlo en Streamlit
-    map_html = folium.Map(location=[19.4326, -99.1332], zoom_start=10).get_root().render()
-    return map_html
-
-# Creamos una página de Streamlit para mostrar el mapa
-st.title("Ejemplo de mapa con Streamlit y Folium")
-st.markdown("Este es un ejemplo de cómo crear un mapa con Streamlit y Folium.")
+# Creamos una página de Streamlit para mostrar la interfaz gráfica
+st.title("Cálculo del número de células por mililitro")
+st.markdown("Este es un ejemplo de cómo calcular el número de células por mililitro a partir del número de células contadas y el factor de dilución.")
 st.markdown("---")
-map_html = generate_map()
-st.markdown(map_html, unsafe_allow_html=True)
+
+# Introducción manual del número de células contadas y el factor de dilución
+cells_counted = st.number_input("Introduce el número de células contadas por cuadrante: ")
+dilution_factor = st.number_input("Introduce el factor de dilución de la muestra: ")
+
+# Calculamos el número de células por mililitro y lo mostramos en la página
+if st.button("Calcular"):
+    result = cells_per_ml(cells_counted, dilution_factor)
+    st.write(f"El número de células por mililitro es: {result}")
+
 
 
 
