9c8091a7b52290808402c8c452b3921c4a6b788a
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2023-05-11 23:24:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.write('Calculadora de Celulas por mililitro, *World!* :sunglasses:')
 def cells_per_ml():
     # Introducción manual del número de células contadas y el factor de dilución
-    cells_counted = title = st.text_input('Celulas en total por los cuadrantes: ')
-    dilution_factor = title = st.text_input('Introduce el factor de dilución de la muestra: ')
+    cells_counted = st.text_input('Celulas en total por los cuadrantes: ')
+    dilution_factor = st.text_input('Introduce el factor de dilución de la muestra: ')
     
     # Cálculo del número de células por mililitro
     cells_per_mm2 = cells_counted * 4  # Se cuentan las células en 4 cuadrantes
