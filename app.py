ac203e5198eb437e7b8e60818e36d04c84c17523
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2023-05-11 23:47:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-with st.sidebar:
-    st.[element_name]
-    
+import streamlit as st
+
+# Agregar una barra lateral
+options = ["Opción 1", "Opción 2", "Opción 3"]
+selected_option = st.sidebar.selectbox("Selecciona una opción", options)
+
+# Agregar widgets a la página principal
+st.write(f"Has seleccionado: {selected_option}")
+st.write("Este es el contenido de la página principal")
 
 
