2b2d4dfd1b688e296873fdc1dffd39362e1f1299
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2023-05-11 23:44:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 import streamlit as st
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-def cells_per_ml(cells_counted, dilution_factor):
+import matplotlib.pyplot as plt
+
+def cells_per_ml():
+    # Introducción manual del número de células contadas y el factor de dilución
+    cells_counted = st.number_input("Introduce el número de células contadas por cuadrante: ")
+    dilution_factor = st.number_input("Introduce el factor de dilución de la muestra: ")
+    
     # Cálculo del número de células por mililitro
     cells_per_mm2 = cells_counted * 4  # Se cuentan las células en 4 cuadrantes
     cells_per_ml = cells_per_mm2 * 10**4 * dilution_factor
+    
+    # Gráfica de los datos ingresados
+    days = st.text_input("Ingrese los días contados separados por comas:")
+    days_list = [int(x) for x in days.split(",")]
+    plt.plot(days_list, [cells_per_ml]*len(days_list), 'ro')
+    plt.xlabel('Días')
+    plt.ylabel('Número de células')
+    st.sidebar.pyplot()
+    
     return cells_per_ml
 
-# Creamos una página de Streamlit para mostrar la interfaz gráfica
-st.title("Cálculo del número de células por mililitro")
-st.markdown("Este es un ejemplo de cómo calcular el número de células por mililitro a partir del número de células contadas y el factor de dilución.")
-st.markdown("---")
+# Crear una gráfica vacía en la barra lateral
+st.sidebar.subheader("Gráfica de células por día")
+plt.figure()
+st.sidebar.pyplot()
 
-# Introducción manual del número de células contadas y el factor de dilución
-cells_counted = st.number_input("Introduce el número de células contadas por cuadrante: ")
-dilution_factor = st.number_input("Introduce el factor de dilución de la muestra: ")
+# Ejemplo de uso
+result = cells_per_ml()
+st.write(f"El número de células por mililitro es: {result}")
 
-# Calculamos el número de células por mililitro y lo mostramos en la página
-if st.button("Calcular"):
-    result = cells_per_ml(cells_counted, dilution_factor)
-    st.write(f"El número de células por mililitro es: {result}")
 
 
 
